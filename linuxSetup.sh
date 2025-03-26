2ecec16939f3875d7bfa075220a423b7915bb6be
Jakub Sanecki <115787745+jsan82@users.noreply.github.com>
2025-03-26 22:22:50 +0100

    a

--- linuxSetup.sh
+++ linuxSetup.sh
@@ -1,8 +1,18 @@
 #! /bin/sh
 # This script is used to create basic setup for Arch with bspwm 
 
-sudo pacman -S yay
+# Dirs
+DIR=`pwd`
+FDIR="$HOME/.local/share/fonts"
+
+
 yay -S picom nitrogen rofi dunst polybar alacritty thunar firefox flameshot alsa-lib alsa-utils pulseaudio pulseaudio-alsa pavucontrol xorg xorg-xinit xorg-server xorg-xrandr xorg-xsetroot xorg-xset
 
-# Copying the config files from current repo to the .config directory
-sudo cp -r config/* ~/.config/
+# Copying the config files from current repo to the .config dir
+cp -r "$(pwd)/.config" ~/
+
+# Installing fonts
+[[ ! -d "$FDIR" ]]&& mkdir -p "$FDIR"
+cp -rf $DIR/fonts/* "$FDIR"
+
+
